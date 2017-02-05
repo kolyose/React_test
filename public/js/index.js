f3e9ed30b9a2d8c556709e7643cd3e1417615ea5
kolyose <kolyose@gmail.com>
2017-02-05 22:44:34 +0200

    bootstrap styling added to admin panel view

--- public/js/index.js
+++ public/js/index.js
@@ -4,39 +4,57 @@ class AdminPanel extends React.Component {
     }
 
     render(){
-        return (            
-            <form>
-                <fieldset>
-                    <legend>Puzzle game settings</legend>
-                    <label> 
-                        Image to play with: <br/>
-                            <input type="file" name="file" accept="image/*" required/>
-                    </label> <br/><br/>
-                    <div>
-                        Game area dimensions: <br/>
-                          <input type="number" name="width" placeholder="Width" min="1000" max="2000" required/>
-                          <span> x </span>
-                          <input type="number" name="height" placeholder="Height" min="1000" max="2000" required/>
-                    </div> <br/><br/>
-                    <label> 
-                        Time limit in seconds: <br/>
-                            <input type="number" name="time" min="1" required/>
-                    </label> <br/><br/>
-                    <div>
-                        Fragments number: <br/>
-                        <select name="fragments">
-                            {this.props.fragmentsOptions.map(fragments => 
-                                <option 
-                                    key={fragments} 
-                                    value={fragments}>
-                                    {fragments}
-                                </option>
-                            )}
-                        </select>
-                    </div> <br/><br/>
-                    <input type="submit" value="Submit"></input>
-                </fieldset>                
-            </form>
+        return (           
+            <div className="col-md-2">         
+                <form>
+                    <fieldset>
+                        <legend>Puzzle game settings</legend>
+                        <div className="form-group">  
+                            <label for="file" className="col-form-label">Load image:</label> 
+                            <input type="file" id="file" name="file" className="form-control-file" accept="image/*" required/>
+                        </div>
+                        <br/>
+
+                        <label>Game area dimensions:</label>
+                        <div className="form-group row">                            
+                            <div className="col-sm-5">
+                                <input type="number" name="width" className="form-control" placeholder="Width" min="1000" max="2000" required/>
+                            </div>
+                            <span className="col-sm-2 form-control-static">x</span>
+                            <div className="col-sm-5">
+                                <input type="number" name="height" className="form-control" placeholder="Height" min="1000" max="2000" required/>
+                            </div>
+                        </div> 
+                        <br/>
+                        
+                        <div className="form-group row">
+                            <label for="time" className="col-form-label col-sm-5"> Time limit (seconds): </label>
+                            <div className="col-sm-6"> 
+                                <input type="number" id="time" name="time" className="form-control" min="1" required/>
+                            </div>
+                        </div>                       
+                        <br/>
+                       
+                        <div className="form-group row">
+                            <label for="fragments" className="col-form-label col-sm-5">Fragments number:</label> 
+                            <div className="col-sm-5">
+                                <select name="fragments" id="fragments" className="form-control">
+                                    {this.props.fragmentsOptions.map(fragments => 
+                                        <option 
+                                            key={fragments} 
+                                            value={fragments}>
+                                            {fragments}
+                                        </option>
+                                    )}
+                                </select>
+                            </div>
+                        </div> 
+                        <br/>
+
+                        <input type="submit" value="Submit" className="btn btn-primary"></input>
+                    </fieldset>                
+                </form>
+            </div>
         );
     }
 }
